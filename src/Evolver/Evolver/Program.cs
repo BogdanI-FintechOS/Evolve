ec7abf189dc00e09fe9b2dcfae2f20574c8b683b
Bogdan Iosif @ Fintech OS <35227468+BogdanIosif-FintechOS@users.noreply.github.com>
2018-01-08 18:08:27 +0200

    Init Evolver

---
+++ src/Evolver/Evolver/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Evolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{{");
+            var cnn = new SqlConnection("Server=192.168.15.60;Database=TestEvlv20171228a;Integrated Security=true;");
+            var evolve = new Evolve.Evolve(cnn, msg => Console.WriteLine(msg))
+            {
+                Locations = new List<string> { @"sql\migration", @"sql\dataset" },
+                MustEraseOnValidationError = true,
+                MetadataTableName = "DB_VERSION"
+            };
+
+            try
+            {
+                evolve.Migrate();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("ERROR: " + exc);
+            }
+            Console.WriteLine("}}");
+            Console.ReadLine();
+        }
+    }
+}
